cc88aeb449fce2623b7bd431fc01a025aed7f9c9
Ishu <ishitasagar2006@gmail.com>
2026-04-02 03:47:47 +0530

    Added GitHub OAuth

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI, HTTPException
 from backend.github_service import get_user_repos
 from backend.analyzer import analyze_repos, generate_suggestions
 from fastapi.middleware.cors import CORSMiddleware
+import httpx
+from fastapi.responses import RedirectResponse
+
+CLIENT_ID = "Ov23liuu5aXrrJ3iM0dK"
+CLIENT_SECRET = "9e26c42eb315033738845e26e4e64550e91fa34d"
 
 app = FastAPI(
     title="DevTrack AI",
@@ -18,12 +23,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ✅ Health check (important for deployment platforms)
+# Health check (important for deployment platforms)
 @app.get("/")
 def health_check():
     return {"status": "ok", "message": "Server is running"}
 
-# ✅ Main API
+# Login Route
+@app.get("/auth/github")
+def github_login():
+    github_url = f"https://github.com/login/oauth/authorize?client_id={CLIENT_ID}"
+    return RedirectResponse(github_url)
+
+# Main API
 @app.get("/user/{username}")
 def get_user(username: str):
     try:
@@ -62,3 +73,36 @@ def get_user(username: str):
             status_code=500,
             detail=f"Internal Server Error: {str(e)}"
         )
+    
+# Callback Route
+
+@app.get("/auth/github/callback")
+async def github_callback(code: str):
+
+    async with httpx.AsyncClient() as client:
+        token_res = await client.post(
+            "https://github.com/login/oauth/access_token",
+            headers={"Accept": "application/json"},
+            data={
+                "client_id": CLIENT_ID,
+                "client_secret": CLIENT_SECRET,
+                "code": code
+            }
+        )
+
+    token_json = token_res.json()
+    access_token = token_json.get("access_token")
+
+    async with httpx.AsyncClient() as client:
+        user_res = await client.get(
+            "https://api.github.com/user",
+            headers={"Authorization": f"token {access_token}"}
+        )
+
+    user_data = user_res.json()
+
+    return {
+        "username": user_data["login"],
+        "avatar": user_data["avatar_url"],
+        "profile": user_data["html_url"]
+    }
